cf6a342232edf6f56c28cc028e555998d7c832cd
Tim <legocacherx@gmail.com>
2016-04-05 12:56:50 -0700

    Generated project with android integration

---
+++ MacroMachine/android/src/com/derelictech/macromachine/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.derelictech.macromachine;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.derelictech.macromachine.MacroMachine;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MacroMachine(), config);
+	}
+}
