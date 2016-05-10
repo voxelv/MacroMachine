1762dcf70c2475dde4c045ca2fc8a1f77165f674
Tim Slippy, voxelv <slippytim@gmail.com>
2016-05-09 18:20:12 -0700

    work on assets thingy...

--- MacroMachine/core/src/com/derelictech/macromachine/util/Assets.java
+++ MacroMachine/core/src/com/derelictech/macromachine/util/Assets.java
@@ -28,6 +28,28 @@ public class Assets implements Disposable{
     private Array<Sound> sounds = new Array<Sound>();
     private Array<Music> musics = new Array<Music>();
 
+    public static Array<TextureRegion> cellCloseFrames;
+    public static TextureRegion basicMaterial;
+    public static TextureRegion basicMaterialIcon;
+    public static TextureRegion metalicMaterial;
+    public static TextureRegion metalicMaterialIcon;
+    public static TextureRegion radicalMaterial;
+    public static TextureRegion radicalMaterialIcon;
+    public static TextureRegion control_unit;
+    public static TextureRegion drillTexture;
+    public static Array<TextureRegion> drill_extend;
+    public static TextureRegion e_battery;
+    public static TextureRegion generator;
+    public static TextureRegion hull_upgrade;
+    public static TextureRegion proximity_detector;
+    public static TextureRegion proximity_dish;
+    public static Array<TextureRegion> wire;
+    public static TextureRegion cell_edge2_pad1;
+    public static TextureRegion game_grid_edge5_pad3;
+    public static TextureRegion hud_bg;
+
+
+
     /**
      * Able to be called to get the textures again
      */
@@ -52,6 +74,27 @@ public class Assets implements Disposable{
         sounds.add(move);
         radical_explosion = Gdx.audio.newSound(Gdx.files.internal("sfx/radical_explosion.wav"));
         sounds.add(radical_explosion);
+
+        cellCloseFrames = new Array<TextureRegion>(atlas.findRegions("cell_anim/cell_edge2_pad1"));
+        basicMaterial = new TextureRegion(atlas.findRegion("materials/basic"));
+        basicMaterialIcon = new TextureRegion(atlas.findRegion("materials/basic_resource"));
+        metalicMaterial = new TextureRegion(atlas.findRegion("materials/metalic"));
+        metalicMaterialIcon = new TextureRegion(atlas.findRegion("materials/metalic_resource"));
+        radicalMaterial = new TextureRegion(atlas.findRegion("materials/radical"));
+        radicalMaterialIcon = new TextureRegion(atlas.findRegion("materials/radical_resource"));
+        control_unit = new TextureRegion(atlas.findRegion("units/control_unit"));
+        drillTexture = new TextureRegion(atlas.findRegion("units/drillTexture"));
+        drill_extend = new Array<TextureRegion>(atlas.findRegions("units/drill_extend"));
+        e_battery = new TextureRegion(atlas.findRegion("units/e_battery"));
+        generator = new TextureRegion(atlas.findRegion("units/generator"));
+        hull_upgrade = new TextureRegion(atlas.findRegion("units/hull_upgrade"));
+        proximity_detector = new TextureRegion(atlas.findRegion("units/proximity_detector"));
+        proximity_dish = new TextureRegion(atlas.findRegion("units/proximity_dish"));
+        wire = new Array<TextureRegion>(atlas.findRegions("units/wire"));
+
+        cell_edge2_pad1 = new TextureRegion(atlas.findRegion("cell_edge2_pad1"));
+        game_grid_edge5_pad3 = new TextureRegion(atlas.findRegion("game_grid_edge5_pad3"));
+        hud_bg = new TextureRegion(atlas.findRegion("hud_bg"));
     }
 
     /**
@@ -60,13 +103,25 @@ public class Assets implements Disposable{
      * @return Returns the Texture region from the atlas specified by name
      */
     public TextureRegion getRegion(String name) {
-        if(atlas != null) {
-            for (TextureAtlas.AtlasRegion r : atlas.getRegions()) {
-                if (r.name.equals(name)) {
-                    return r;
-                }
-            }
-        }
+        if(name.equals("materials/basic")) return basicMaterial;
+        if(name.equals("materials/basic_resource")) return basicMaterialIcon;
+        if(name.equals("materials/metalic")) return metalicMaterial;
+        if(name.equals("materials/metalic_resource")) return metalicMaterialIcon;
+        if(name.equals("materials/radical")) return radicalMaterial;
+        if(name.equals("materials/radical_resource")) return radicalMaterialIcon;
+        if(name.equals("units/control_unit")) return control_unit;
+        if(name.equals("units/drillTexture")) return drillTexture;
+        if(name.equals("units/e_battery")) return e_battery;
+        if(name.equals("units/generator")) return generator;
+        if(name.equals("units/hull_upgrade")) return hull_upgrade;
+        if(name.equals("units/proximity_detector")) return proximity_detector;
+
+        if(name.equals("units/proximity/dish")) return proximity_dish;
+        if(name.equals("cell_edge2_pad1")) return cell_edge2_pad1;
+        if(name.equals("game_grid_edge5_pad3")) return game_grid_edge5_pad3;
+        if(name.equals("hud_bg")) return hud_bg;
+
+
         return null;
     }
 
@@ -76,8 +131,9 @@ public class Assets implements Disposable{
      * @return Returns the {@link Array<TextureRegion>}
      */
     public Array<TextureRegion> getFrameSequence(String frame_sequence_name) {
-        Array<TextureAtlas.AtlasRegion> atlasRegions = atlas.findRegions(frame_sequence_name);
-        return new Array<TextureRegion>(atlasRegions);
+        if(frame_sequence_name.equals("cell_anim/cell_edge2_pad1")) return cellCloseFrames;
+        if(frame_sequence_name.equals("units/wire")) return wire;
+        return null;
     }
 
     public void playDrillSound() {
